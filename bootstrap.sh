a5b7cbb57e1a1d10db32d812b11ab9e9aca4cae3
WhatTheFar <jakpat.m@gmail.com>
2019-07-13 13:19:42 +0700

    refactor: remove dot files to config directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,18 +4,15 @@ RESET=`tput sgr0`
 GREEN=`tput setaf 32`
 BLUE=`tput setaf 34`
 
-dotfiles=`ls  .[^.]*`
-exclude=( ".gitignore" ".git:")
+dotfiles=`find . -type f -name "*" -path "./config/*" | sed 's/^\.\/config\///'`
+# dotfiles=`find . -type f -name "*" -path "./config/*" | sed 's/^\.\/config\///'`
 
 echo "> Initializing..."
 echo
 
 for file in $dotfiles
 do
-    if [[ ! " ${exclude[@]} " =~ " ${file} " ]]; then
-        echo -e "$BLUE--- Linking $file ---$RESET"
-        echo
-        ln -sf "$(pwd)/$file" "$HOME/$file"
-        echo
-    fi
+    echo -e "$BLUE--- Linking $file ---$RESET"
+    # rm "$HOME/$file"
+    ln -sf "$(pwd)/config/$file" "$HOME/$file"
 done
