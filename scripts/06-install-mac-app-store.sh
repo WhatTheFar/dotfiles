dc1b6b7a349b17b582984a8b3a59fcfb6232680a
WhatTheFar <jakpat.m@gmail.com>
2019-07-16 01:53:18 +0700

    sctipts: install mac app store applications

---
+++ scripts/06-install-mac-app-store.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+RESET=`tput sgr0`
+BLUE=`tput setaf 30`
+
+# install macos apps
+mas install 937984704 # Amphetamine
+mas install 990588172 # Gestimer
+mas install 539883307 # LINE
+mas install 441258766 # Magnet
