04aaaae37c5f53e65f9b2c98445e18f2c4d0901a
WhatTheFar <jakpat.m@gmail.com>
2020-09-08 12:31:42 +0700

    scripts: install dcp auto complete based on current version

--- scripts/05-install-bash-completion.sh
+++ scripts/05-install-bash-completion.sh
@@ -3,5 +3,7 @@
 RESET=`tput sgr0`
 BLUE=`tput setaf 30`
 
+VERSION=$(docker-compose version --short)
+
 # Install dokcer bash completion for homebrew docker
-sudo curl -L https://raw.githubusercontent.com/docker/compose/1.24.0/contrib/completion/bash/docker-compose -o "$(brew --prefix)/etc/bash_completion.d/docker-compose"
+sudo curl -L https://raw.githubusercontent.com/docker/compose/$VERSION/contrib/completion/bash/docker-compose -o "$(brew --prefix)/etc/bash_completion.d/docker-compose"
