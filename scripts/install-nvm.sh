09a4594829ee6bb08bbb7eb42c38acacb83627d0
WhatTheFar <jakpat.m@gmail.com>
2020-10-11 18:03:23 +0700

    feat(script): install volta

--- scripts/install-nvm.sh
+++ scripts/install-nvm.sh
@@ -14,3 +14,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # https://github.com/nvm-sh/nvm
 echo -e "$BLUE--- Installing nvm ---$RESET"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# install volta
+curl https://get.volta.sh | bash -s -- --skip-setup
+# run volta setup to manually configure your shell to use Volta
