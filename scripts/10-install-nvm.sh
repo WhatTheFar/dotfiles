70884ef50335c0d9412ad5fa7e1399cfe5c1bd0b
WhatTheFar <jakpat.m@gmail.com>
2021-02-01 20:57:01 +0700

    scripts(nvm): docs for installing volta

--- scripts/10-install-nvm.sh
+++ scripts/10-install-nvm.sh
@@ -16,5 +16,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 # install volta
+echo -e "$BLUE--- Installing volta ---$RESET"
+# '--skip-setup' Do not run 'volta setup' to modify startup scripts
 curl https://get.volta.sh | bash -s -- --skip-setup
 # run volta setup to manually configure your shell to use Volta
