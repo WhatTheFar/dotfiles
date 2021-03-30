8febbdbada3d978280c40a662a848106cb839e43
WhatTheFar <jakpat.m@gmail.com>
2021-03-31 03:42:45 +0700

    scripts: support linking directory

--- scripts/00-linking-dotfiles.sh
+++ scripts/00-linking-dotfiles.sh
@@ -3,13 +3,53 @@
 RESET=`tput sgr0`
 BLUE=`tput setaf 30`
 
-dir="src"
-dotfiles=`find . -type f -name "*" -path "./${dir}/*" | sed "s/^\.\/${dir}\///"`
+src="src"
+entire_dirs=(
+    '.config/karabiner'
+    '.hammerspoon'
+)
+
+dotfiles=`find . -type f -name "*" -path "./${src}/*" | sed "s/^\.\/${src}\///"`
+linked_dirs=()
+linked_dirs_re=""
+
+path_to_regex() {
+    echo $@ | sed -e 's/\([\/\.]\)/\\\1/g'
+}
 
 for file in $dotfiles
 do
-    echo -e "$BLUE--- Linking $file ---$RESET"
+    if [[ "$file" =~ ${linked_dirs_re} ]]; then
+        echo -e "$BLUE--- Skipping $file ---$RESET"
+        continue
+    fi
 
+    skip=""
+    for entire_dir in $entire_dirs
+    do
+        re="^$(path_to_regex $entire_dir)\\/.*\$"
+        if [[ "$file" =~ $re ]]; then
+            echo -e "$BLUE--- Linking $entire_dir ---$RESET"
+            # mkdir -p "$HOME/$entire_dir"
+            rm -rf "$HOME/$entire_dir"
+            ln -s "$(pwd)/$src/$entire_dir" "$HOME/$entire_dir"
+
+
+            echo -e "$BLUE--- Skipping $file ---$RESET"
+
+            linked_dirs=("${linked_dirs[@]}" "${entire_dir}")
+            linked_dirs_re="^($(echo "${linked_dirs[@]}" | sed -e 's/\([\/\.]\)/\\\1/g' -e 's/ /|/g')).*$"
+            skip=true
+
+            break
+        fi
+    done
+    if [[ -n $skip ]]; then
+        continue
+    fi
+
+    echo -e "$BLUE--- Linking $file ---$RESET"
     mkdir -p "$HOME/$(dirname $file)"
-    ln -sf "$(pwd)/$dir/$file" "$HOME/$file"
+    ln -sf "$(pwd)/$src/$file" "$HOME/$file"
 done
+
