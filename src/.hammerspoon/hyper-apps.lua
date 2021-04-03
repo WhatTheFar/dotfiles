fb6d944b814176f63c0255ebc7562679899b6ed4
WhatTheFar <jakpat.m@gmail.com>
2021-04-04 02:51:34 +0700

    style(spoon): sort hyper apps

--- src/.hammerspoon/hyper-apps.lua
+++ src/.hammerspoon/hyper-apps.lua
@@ -1,10 +1,10 @@
 return {
-  { 'm', 'Spotify' },           -- "M" for "Music"
   { 'b', 'Google Chrome' },     -- "B" for "Browser"
   { 'c', 'Line' },              -- "C" for "Chat"
   { 'd', 'Discord' },           -- "D" for "Discord"
   { 'e', 'Visual Studio Code' },-- "E" for "Editor"
   { 'f', 'Fork' },              -- "F" for "Fork"
+  { 'm', 'Spotify' },           -- "M" for "Music"
   { 's', 'Slack' },             -- "S" for "Slack"
   { 't', 'iTerm' },             -- "T" for "Terminal"
 }
