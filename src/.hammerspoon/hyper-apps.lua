8bf8c9a6c346dd3baa62ff860fe36681026e74a5
WhatTheFar <jakpat.m@gmail.com>
2021-04-03 21:29:37 +0700

    feat(spoon): hyper keys for quickly lunching apps

---
+++ src/.hammerspoon/hyper-apps.lua
@@ -0,0 +1,9 @@
+return {
+  { 'm', 'Spotify' },           -- "M" for "Music"
+  { 'b', 'Google Chrome' },     -- "B" for "Browser"
+  { 'f', 'Fork' },              -- "F" for "Fork"
+  { 'c', 'Line' },              -- "C" for "Chat"
+  { 's', 'Slack' },             -- "S" for "Slack"
+  { 'd', 'Discord' },           -- "D" for "Discord"
+  { 't', 'iTerm' },             -- "T" for "Terminal"
+}
