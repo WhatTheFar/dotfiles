2be090292957535ccf32cff31f0c672106181435
WhatTheFar <jakpat.m@gmail.com>
2021-04-17 00:33:52 +0700

    fix(nvim): telescope dotfiles using git_files

--- src/.config/nvim/lua/whatthefar/telescope.lua
+++ src/.config/nvim/lua/whatthefar/telescope.lua
@@ -1,6 +1,6 @@
 local M = {}
 M.search_dotfiles = function()
-    require("telescope.builtin").find_files({
+    require("telescope.builtin").git_files({
         prompt_title = "< VimRC >",
         cwd = "$HOME/Projects/what_the_far/dotfiles/",
     })
