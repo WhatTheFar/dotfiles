2184a5fa4754520b7712fa96073caa8cd32a5bec
WhatTheFar <jakpat.m@gmail.com>
2021-08-06 18:35:47 +0700

    style: format bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-RESET=`tput sgr0`
-GREEN=`tput setaf 32`
-BLUE=`tput setaf 34`
+RESET=$(tput sgr0)
+GREEN=$(tput setaf 32)
+BLUE=$(tput setaf 34)
 
 dir="scripts"
 # scripts=`find . -type f -name "*.sh" -path "./${dir}/*" | sed "s/^\.\/${dir}\///" | grep "^\d" | sort -n`
-scripts=`ls ${dir}/*.sh | sed "s/^${dir}\///" | grep "^\d"`
+scripts=$(ls ${dir}/*.sh | sed "s/^${dir}\///" | grep "^\d")
 
 echo "> Initializing..."
 echo
 
-for script in $scripts
-do
-    echo -e "$BLUE--- Executing $script ---$RESET"
-    echo
+for script in $scripts; do
+	echo -e "$BLUE--- Executing $script ---$RESET"
+	echo
 
-    $dir/$script
+	$dir/$script
 
-    echo
-    echo -e "$GREEN--- Finished executing $script ---$RESET"
-    echo
+	echo
+	echo -e "$GREEN--- Finished executing $script ---$RESET"
+	echo
 done
+
