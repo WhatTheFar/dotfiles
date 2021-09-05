9af6e0049a07ad98e5986f2760df77582c55e789
WhatTheFar <jakpat.m@gmail.com>
2021-09-05 17:51:27 +0700

    feat(lvim): add nvim-lsp-ts-utils

--- src/.config/lvim/config.lua
+++ src/.config/lvim/config.lua
@@ -246,6 +246,60 @@ lvim.lang.typescript.linters = {
 		args = {},
 	},
 }
+-- TODO: enhance nvim-lsp-ts-utils code actions
+local lsp = require "lsp"
+lvim.lang.typescript.lsp.setup.on_attach = function(client, bufnr)
+	lsp.common_on_attach(client, bufnr)
+
+	-- disable tsserver formatting if you plan on formatting via null-ls
+	client.resolved_capabilities.document_formatting = false
+	client.resolved_capabilities.document_range_formatting = false
+
+	local ts_utils = require "nvim-lsp-ts-utils"
+
+	-- defaults
+	ts_utils.setup {
+		debug = false,
+		disable_commands = false,
+		enable_import_on_completion = false,
+
+		-- import all
+		import_all_timeout = 5000, -- ms
+		import_all_priorities = {
+			buffers = 4, -- loaded buffer names
+			buffer_content = 3, -- loaded buffer content
+			local_files = 2, -- git files or files with relative path markers
+			same_file = 1, -- add to existing import statement
+		},
+		import_all_scan_buffers = 100,
+		import_all_select_source = false,
+
+		-- eslint
+		eslint_enable_code_actions = true,
+		eslint_enable_disable_comments = true,
+		eslint_bin = "eslint",
+		eslint_config_fallback = nil,
+		eslint_enable_diagnostics = false,
+		eslint_show_rule_id = false,
+
+		-- formatting
+		enable_formatting = false,
+		formatter = "prettier",
+		formatter_config_fallback = nil,
+
+		-- update imports on file move
+		update_imports_on_move = false,
+		require_confirmation_on_move = false,
+		watch_dir = nil,
+
+		-- filter diagnostics
+		filter_out_diagnostics_by_severity = {},
+		filter_out_diagnostics_by_code = {},
+	}
+
+	-- required to fix code action ranges and filter diagnostics
+	ts_utils.setup_client(client)
+end
 
 -- lua
 lvim.lang.lua.formatters = {
@@ -402,6 +456,7 @@ lvim.plugins = {
 			require("lsp_signature").setup()
 		end,
 	},
+	{ "jose-elias-alvarez/nvim-lsp-ts-utils" },
 	{
 		"vuki656/package-info.nvim",
 		config = function()
