93a6c8b79d3a162cdb7581d57aa2bc9fa96dd9fc
WhatTheFar <jakpat.m@gmail.com>
2021-10-04 00:28:37 +0700

    refactor(lvim): remove some comments

--- src/.config/lvim/config.lua
+++ src/.config/lvim/config.lua
@@ -383,11 +383,7 @@ lvim.plugins = {
 		"folke/todo-comments.nvim",
 		-- requires = "nvim-lua/plenary.nvim",
 		config = function()
-			require("todo-comments").setup {
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			}
+			require("todo-comments").setup {}
 		end,
 	},
 
