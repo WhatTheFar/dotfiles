3356d8b58e0ce01ebdc94eb4cce632812eaf737e
WhatTheFar <jakpat.m@gmail.com>
2022-02-23 00:11:35 +0700

    feat(lvim): add nvim-treesitter-context

--- src/.config/lvim/config.lua
+++ src/.config/lvim/config.lua
@@ -596,6 +596,15 @@ lvim.plugins = {
 	{ "p00f/nvim-ts-rainbow" },
 	-- already included
 	-- { "JoosepAlviste/nvim-ts-context-commentstring", event = "BufRead" },
+	{
+		"romgrk/nvim-treesitter-context",
+		config = function()
+			require("treesitter-context").setup {
+				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+				throttle = true, -- Throttles plugin updates (may improve performance)
+			}
+		end,
+	},
 
 	-- Language Pack
 	{ "LunarVim/vim-solidity" },
