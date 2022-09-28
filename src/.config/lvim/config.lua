377980754894de9d77c9bd01f6a9e22ba4dbab32
WhatTheFar <jakpat.m@gmail.com>
2022-09-29 02:01:10 +0700

    fix(lvim): remove nvimtree.show_icon.git
    the configuration move to *nvim-tree.renderer.icons.show.git*

--- src/.config/lvim/config.lua
+++ src/.config/lvim/config.lua
@@ -215,7 +215,6 @@ lvim.builtin.which_key.setup.triggers_blacklist = {
 }
 
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 1
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {}
