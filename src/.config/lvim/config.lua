4fa2a5bfbe7f91cc86fd7465a78ce426d9f0ac88
WhatTheFar <jakpat.m@gmail.com>
2022-10-11 01:33:05 +0700

    feat(lvim): add treesitter-textobjects

--- src/.config/lvim/config.lua
+++ src/.config/lvim/config.lua
@@ -226,6 +226,72 @@ lvim.builtin.treesitter.context_commentstring.enable = true
 lvim.builtin.treesitter.context_commentstring.enable_autocmd = false
 lvim.builtin.treesitter.rainbow.enable = true
 
+lvim.builtin.treesitter.textobjects = {
+	select = {
+		enable = true,
+
+		-- Automatically jump forward to textobj, similar to targets.vim
+		lookahead = true,
+
+		keymaps = {
+			-- You can use the capture groups defined in textobjects.scm
+			["af"] = "@function.outer",
+			["if"] = "@function.inner",
+			["ac"] = "@class.outer",
+			-- you can optionally set descriptions to the mappings (used in the desc parameter of nvim_buf_set_keymap
+			["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+		},
+		-- You can choose the select mode (default is charwise 'v')
+		selection_modes = {
+			["@parameter.outer"] = "v", -- charwise
+			["@function.outer"] = "V", -- linewise
+			["@class.outer"] = "<c-v>", -- blockwise
+		},
+		-- If you set this to `true` (default is `false`) then any textobject is
+		-- extended to include preceding xor succeeding whitespace. Succeeding
+		-- whitespace has priority in order to act similarly to eg the built-in
+		-- `ap`.
+		include_surrounding_whitespace = true,
+	},
+	swap = {
+		enable = false,
+		swap_next = {
+			["<leader>a"] = "@parameter.inner",
+		},
+		swap_previous = {
+			["<leader>A"] = "@parameter.inner",
+		},
+	},
+	move = {
+		enable = true,
+		set_jumps = true, -- whether to set jumps in the jumplist
+		goto_next_start = {
+			["]m"] = "@function.outer",
+			["]]"] = "@class.outer",
+		},
+		goto_next_end = {
+			["]M"] = "@function.outer",
+			["]["] = "@class.outer",
+		},
+		goto_previous_start = {
+			["[m"] = "@function.outer",
+			["[["] = "@class.outer",
+		},
+		goto_previous_end = {
+			["[M"] = "@function.outer",
+			["[]"] = "@class.outer",
+		},
+	},
+	lsp_interop = {
+		enable = false,
+		border = "none",
+		peek_definition_code = {
+			["<leader>df"] = "@function.outer",
+			["<leader>dF"] = "@class.outer",
+		},
+	},
+}
+
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
 	"bash",
@@ -594,6 +660,8 @@ lvim.plugins = {
 	{ "p00f/nvim-ts-rainbow" },
 	-- already included
 	-- { "JoosepAlviste/nvim-ts-context-commentstring", event = "BufRead" },
+	{ "nvim-treesitter/nvim-treesitter-textobjects" },
+	-- TODO: {"RRethy/nvim-treesitter-textsubjects"},
 	{
 		"romgrk/nvim-treesitter-context",
 		config = function()
