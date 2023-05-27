3d99986df12a31312e89d52bb93fa59ea663f262
WhatTheFar <jakpat.m@gmail.com>
2023-05-28 04:27:56 +0700

    feat(lvim): disable copilot panel mode

--- src/.config/lvim/config.lua
+++ src/.config/lvim/config.lua
@@ -877,7 +877,10 @@ lvim.plugins = {
 		"zbirenbaum/copilot-cmp",
 		after = { "copilot.lua" },
 		config = function()
-			require("copilot_cmp").setup()
+			require("copilot").setup {
+				suggestion = { enabled = false },
+				panel = { enabled = false },
+			}
 		end,
 	},
 	{
