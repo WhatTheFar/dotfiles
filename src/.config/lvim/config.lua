8461ef6688e5835bb9601648c2c4f3bb0f5cb918
WhatTheFar <jakpat.m@gmail.com>
2023-05-28 04:41:24 +0700

    fix(lvim): use version instead of tag

--- src/.config/lvim/config.lua
+++ src/.config/lvim/config.lua
@@ -479,7 +479,7 @@ lvim.plugins = {
 	{ "tpope/vim-repeat" },
 	{
 		"kylechui/nvim-surround",
-		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
 		config = function()
 			require("nvim-surround").setup {
 				-- Configuration here, or leave empty to use defaults
