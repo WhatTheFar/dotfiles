c6a6c9faeec8320320a583306ef96098c1770f14
WhatTheFar <jakpat.m@gmail.com>
2023-05-31 03:40:04 +0700

    feat(lvim): add lsp-inlayhints but disabled

--- src/.config/lvim/config.lua
+++ src/.config/lvim/config.lua
@@ -1041,6 +1041,56 @@ lvim.plugins = {
 		end,
 	},
 	-- { "folke/trouble.nvim", cmd = "TroubleToggle" },
+	-- {
+	-- 	"lvimuser/lsp-inlayhints.nvim",
+	-- 	config = function()
+	-- 		vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
+	-- 		vim.api.nvim_create_autocmd("LspAttach", {
+	-- 			group = "LspAttach_inlayhints",
+	-- 			callback = function(args)
+	-- 				if not (args.data and args.data.client_id) then
+	-- 					return
+	-- 				end
+
+	-- 				local bufnr = args.buf
+	-- 				local client = vim.lsp.get_client_by_id(args.data.client_id)
+	-- 				require("lsp-inlayhints").on_attach(client, bufnr)
+	-- 			end,
+	-- 		})
+
+	-- 		require("lsp-inlayhints").setup {
+	-- 			inlay_hints = {
+	-- 				parameter_hints = {
+	-- 					show = false,
+	-- 					-- prefix = "<- ",
+	-- 					separator = ", ",
+	-- 				},
+	-- 				type_hints = {
+	-- 					-- type and other hints
+	-- 					show = true,
+	-- 					prefix = "",
+	-- 					separator = ", ",
+	-- 					remove_colon_end = false,
+	-- 					remove_colon_start = false,
+	-- 				},
+	-- 				-- separator between types and parameter hints. Note that type hints are
+	-- 				-- shown before parameter
+	-- 				labels_separator = "  ",
+	-- 				-- whether to align to the length of the longest line in the file
+	-- 				max_len_align = false,
+	-- 				-- padding from the left if max_len_align is true
+	-- 				max_len_align_padding = 1,
+	-- 				-- whether to align to the extreme right or not
+	-- 				right_align = false,
+	-- 				-- padding from the right if right_align is true
+	-- 				right_align_padding = 7,
+	-- 				-- highlight group
+	-- 				highlight = "Comment",
+	-- 			},
+	-- 			debug_mode = false,
+	-- 		}
+	-- 	end,
+	-- },
 	{
 		"ray-x/lsp_signature.nvim",
 		event = "InsertEnter",
