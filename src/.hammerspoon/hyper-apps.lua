1dfb8a01876624f8f1426236dade8e01badf0a38
WhatTheFar <jakpat.m@gmail.com>
2023-08-25 14:04:18 +0700

    feat(hyper): add Figma

--- src/.hammerspoon/hyper-apps.lua
+++ src/.hammerspoon/hyper-apps.lua
@@ -4,11 +4,11 @@ return {
 	{ "d", "Discord" }, -- "D" for "Discord"
 	{ "v", "Voicemod" }, -- "V" for "Voicemod"
 	{ "f", "Fork" }, -- "F" for "Fork"
+	{ "g", "Figma" }, -- "G" for "Graphic???" just near T
 	{ "m", "Spotify" }, -- "M" for "Music"
 	{ "s", "Slack" }, -- "S" for "Slack"
 	{ "t", "iTerm" }, -- "T" for "Terminal"
 	{ "w", "Logseq" }, -- "W" for "Write"
-	{ "r", "Reminders" }, -- "R" for "Reminders"
 
 	{ "b", "Basecamp 3" }, -- "B" for "Basecamp"
 }
