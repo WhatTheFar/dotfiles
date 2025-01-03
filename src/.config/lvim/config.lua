fce0c3cbcbac9a400924d5855b10be720617dfe2
WhatTheFar <jakpat.m@gmail.com>
2025-01-03 11:34:39 +0700

    feat(lvim): add dap

--- src/.config/lvim/config.lua
+++ src/.config/lvim/config.lua
@@ -998,6 +998,59 @@ lvim.plugins = {
 
 	-- Language Pack
 	{ "mfussenegger/nvim-jdtls" },
+	{
+		"mxsdev/nvim-dap-vscode-js",
+		config = function()
+			require("dap-vscode-js").setup {
+				-- NOTE: https://github.com/mxsdev/nvim-dap-vscode-js/issues/58
+				debugger_cmd = { "js-debug-adapter" },
+				adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
+			}
+			for _, language in ipairs { "typescript", "javascript", "svelte" } do
+				require("dap").configurations[language] = {
+					-- attach to a node process that has been started with
+					-- `--inspect` for longrunning tasks or `--inspect-brk` for short tasks
+					-- npm script -> `node --inspect-brk ./node_modules/.bin/vite dev`
+					{
+						-- use nvim-dap-vscode-js's pwa-node debug adapter
+						type = "pwa-node",
+						-- attach to an already running node process with --inspect flag
+						-- default port: 9222
+						request = "attach",
+						-- allows us to pick the process using a picker
+						processId = require("dap.utils").pick_process,
+						-- name of the debug action you have to select for this config
+						name = "Attach debugger to existing `node --inspect` process",
+						-- for compiled languages like TypeScript or Svelte.js
+						sourceMaps = true,
+						-- resolve source maps in nested locations while ignoring node_modules
+						resolveSourceMapLocations = {
+							"${workspaceFolder}/**",
+							"!**/node_modules/**",
+						},
+						-- path to src in vite based projects (and most other projects as well)
+						cwd = "${workspaceFolder}/src",
+						-- we don't want to debug code inside node_modules, so skip it!
+						skipFiles = { "${workspaceFolder}/node_modules/**/*.js" },
+					},
+					-- only if language is javascript, offer this debug action
+					language == "javascript"
+							and {
+								-- use nvim-dap-vscode-js's pwa-node debug adapter
+								type = "pwa-node",
+								-- launch a new process to attach the debugger to
+								request = "launch",
+								-- name of the debug action you have to select for this config
+								name = "Launch file in new node process",
+								-- launch current file
+								program = "${file}",
+								cwd = "${workspaceFolder}",
+							}
+						or nil,
+				}
+			end
+		end,
+	},
 	-- {
 	-- 	"iamcco/markdown-preview.nvim",
 	-- 	run = "cd app && npm install",
